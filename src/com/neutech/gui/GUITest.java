6239b8aa8f8f943bcf590740ee2d90eec5588bb6
Black21 <2427265846@qq.com>
2020-11-24 16:53:38 +0800

    test

---
+++ src/com/neutech/gui/GUITest.java
@@ -0,0 +1,22 @@
+package com.neutech.gui;
+
+import java.awt.*;
+
+/**
+ * 生成窗口文件
+ * 1.继承frame类
+ * 2.重写paint（Graphic g）
+ */
+public class GUITest extends Frame {
+
+    public void loadFrame(){
+        this.setSize(1000,800);
+        this.setLocation(0,0);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new GUITest().loadFrame();
+    }
+}
